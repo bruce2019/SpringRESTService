7de74222521e83cd018a5154b6ccef04463aecee
RishiRaj <rishiraj.sahu@topcoder.com>
2019-01-21 23:26:06 +0530

    removed unused code

--- src/main/java/com/glaucus/controller/CountController.java
+++ src/main/java/com/glaucus/controller/CountController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.glaucus.exception.ResourceNotFoundException;
-import com.glaucus.repository.CounterRepository;
 import com.glaucus.service.CounterService;
 
 /**
@@ -23,10 +22,6 @@ import com.glaucus.service.CounterService;
 public class CountController {
 
 	@Autowired
-	@Qualifier("counterRepository")
-	private CounterRepository counterRepository;
-
-	@Autowired
 	@Qualifier("counterService")
 	private CounterService counterService;
 
@@ -35,26 +30,4 @@ public class CountController {
 		int rows = counterService.updateNative();
 		return ResponseEntity.ok().body(rows);
 	}
-
-    /*@DeleteMapping("/counter/{id}")
-    public Map<String, Boolean> deleteCounter(@PathVariable(value = "id") Long counterId) throws ResourceNotFoundException {
-        Counter counter = counterRepository.findById(counterId)
-       .orElseThrow(() -> new ResourceNotFoundException("Counter not found for this id :: " + counterId));
-        counterRepository.delete(counter);
-        Map<String, Boolean> response = new HashMap<>();
-        response.put("deleted", Boolean.TRUE);
-        return response;
-    }
-
-	@GetMapping("/counter/{id}")
-	public ResponseEntity<Counter> getCounterById(@PathVariable(value = "id") Long counterId) throws ResourceNotFoundException {
-		System.out.println("controller called : getCounterById");
-		Counter counter = counterService.getCounter(counterId);
-		return ResponseEntity.ok().body(counter);
-	}
-
-	@PostMapping("/counter")
-	public Counter createCounter(@Valid @RequestBody Counter counter) {
-		return counterService.createCounter(counter);
-	}*/
 }
--- src/main/java/com/glaucus/service/CounterService.java
+++ src/main/java/com/glaucus/service/CounterService.java
@@ -7,7 +7,6 @@ import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.glaucus.exception.ResourceNotFoundException;
-import com.glaucus.model.Counter;
 import com.glaucus.repository.CounterRepository;
 
 /**
@@ -21,30 +20,8 @@ public class CounterService {
 	@Autowired
 	CounterRepository counterRepository;
 
-	@Transactional(isolation=Isolation.SERIALIZABLE)
-	public Counter update(Long id) throws ResourceNotFoundException {
-		return updateCounter(id);
-	}
-
-	private Counter updateCounter(Long id) throws ResourceNotFoundException {
-		Counter counter = counterRepository.findById(id)
-				.orElseThrow(() -> new ResourceNotFoundException("Counter not found for this id :: " + id));
-		counter.setCount(1 + counter.getCount());
-		final Counter updatedCounter = counterRepository.save(counter);
-		return updatedCounter;
-	}
-
 	@Transactional(isolation=Isolation.DEFAULT)
 	public int updateNative() throws ResourceNotFoundException {
 		return counterRepository.incrementCount();
 	}
-	
-	/*public Counter getCounter(Long id) throws ResourceNotFoundException {
-        Counter counter = counterRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Counter not found for this id :: " + id));
-        return counter;
-	}
-	
-	public Counter createCounter(Counter counter) {
-		return counterRepository.save(counter);
-	}*/
 }
